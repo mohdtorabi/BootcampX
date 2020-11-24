7139381e9ed00691257edc7e6b04c62f1962b49b
mohdtorabi <mitq8.mt@gmail.com>
2020-11-24 03:05:07 +0000

    Completed 3_queries

--- 3_queries_group-by/3_low_average.sql
+++ 3_queries_group-by/3_low_average.sql
@@ -4,4 +4,5 @@ JOIN assignment_submissions ON student_id = students.id
 JOIN assignments ON assignments.id = assignment_id
 WHERE end_date IS NULL
 GROUP BY student
+HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
 ORDER BY average_assignment_duration;
